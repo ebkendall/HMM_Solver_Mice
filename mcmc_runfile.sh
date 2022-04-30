ecded23ce5c1a0cea9b20c79be275598dc0a2a89
ebkendall <ebkendal@ncsu.edu>
2022-04-30 14:49:53 -0400

    added to the UF and NCSU cluster

--- mcmc_runfile.sh
+++ mcmc_runfile.sh
@@ -1,12 +1,12 @@
 #!/bin/tcsh
 
-#BSUB -J jobName[1-10]       #job name AND job array
-#BSUB -n 1                   #number of cores: generally only need 1 to 5
+#BSUB -J mice[1-15]       #job name AND job array
+#BSUB -n 16                   #number of cores: generally only need 1 to 5
 #BSUB -R span[hosts=1]       #distribute across 1 node
-#BSUB -W 36:00               #walltime limit: hours:minutes
-#BSUB -o /share/.../trial_%I.out  # filepath for the output
-#BSUB -e /share/.../error_%I.err  # filepath for error
+#BSUB -W 48:00               #walltime limit: hours:minutes
+#BSUB -o /share/hmmrs/ebkendal/HMM_Solver_Mice/FINAL/Model_out/a_out/trial1_%I.out  # filepath for the output
+#BSUB -e /share/hmmrs/ebkendal/HMM_Solver_Mice/FINAL/Model_out/a_out/error1_%I.err  # filepath for error
 
 module load R
 
-Rscript r_file_name.r $LSB_JOBINDEX
+Rscript mcmc_runfile.r $LSB_JOBINDEX
